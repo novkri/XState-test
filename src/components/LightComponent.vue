eb015750278e687c8accaa582f678b077a87ebcd
novickay <novickaya@sputnikfund.ru>
2022-06-17 16:26:25 +0700

    frim Machines to Activities

--- src/components/LightComponent.vue
+++ src/components/LightComponent.vue
@@ -1,16 +1,39 @@
 <template>
-  <div>
-    I am representing Light machine
+  <div class="lightswitcher">
+    I am representing Light machine {{state.context.switches }}
+    <div class="round" v-if="state.matches('red')" :style="{background: 'red'}" @click="send('SWITCH')" />
+    <div class="round" v-if="state.matches('yellow')" :style="{background: 'yellow'}" @click="send('SWITCH')" />
+    <div class="round" v-if="state.matches('green')" :style="{background: 'green'}" @click="send('SWITCH')" />
+    <br />
+    <button @click="send('BREAK')">Break</button>
   </div>
 </template>
 
 <script setup lang="ts">
-// import lightMachine from "../machines/light.machine.ts";
-import {onMounted} from "vue";
+import lightMachine from "../machines/light.machine.ts";
+import {useMachine} from "@xstate/vue";
 
-onMounted(() => {
-  console.log('1')
-})
+
+const { state, send } = useMachine(lightMachine)
+
+console.log(state, send)
+
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped  >
+.lightswitcher {
+  display: flex;
+  flex-direction: column;
+}
+.round {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-top: 10px;
+}
+
+button {
+  width: 150px;
+  margin: auto;
+}
+</style>
--- src/machines/light.machine.ts
+++ src/machines/light.machine.ts
@@ -1,6 +1,15 @@
-import {createMachine} from "xstate";
+import {assign, createMachine} from "xstate";
 
+interface LightContext {
+    elapsed: number
+    direction: string
+    switches: number
+}
 const lightMachine = createMachine({
+    // for better  context typing
+    schema: {
+        context: {} as LightContext
+    },
     // identifier
     id: 'light',
 
@@ -10,23 +19,77 @@ const lightMachine = createMachine({
     // local context for entire machine
     context: {
         elapsed: 0,
-        direction: 'east'
+        direction: 'east',
+        switches: 0
     },
 
     // state definitions
     states: {
         green: {
+            tags: ['lightswitcher'],
             // action on state entry
-            entry: 'alertGreen'
+            // @ts-ignore
+            entry: assign({ switches: (ctx) => ctx.switches + 1}),
+
+            on: {
+                SWITCH: {target: 'red'},
+                BREAK: {target: 'broken', cond: 'isTooManySwitches'}
+
+            }
         },
-        yellow: {},
-        red: {}
+        yellow: {
+            tags: ['lightswitcher'],
+            // @ts-ignore
+            entry: assign({ switches: (ctx) => ctx.switches + 1}),
+            on: {
+                SWITCH: 'green',
+                BREAK: {target: 'broken', cond: {
+                        type: 'isTooManySwitches',
+                        minAmount: 4
+                    }}
+
+            }
+        },
+        red: {
+            tags: ['lightswitcher'],
+            // @ts-ignore
+            entry: assign({ switches: (ctx: LightContext) => ctx.switches + 1}),
+            on: {
+                SWITCH: 'yellow',
+                BREAK: {target: 'broken', cond: (context) => context.switches > 7}
+
+            },
+            // exit actions
+            exit: 'logPeople',
+        },
+        broken: {
+            type: 'parallel',
+            tags: ['accident'],
+            states: {
+                peopleStop: {
+                    entry: 'logPeople'
+                },
+                carsStop: {
+                    entry: 'logCars'
+                }
+            },
+            on: {
+                // is activated by any event.
+                "*": 'red'
+            }
+        }
     }
 }, {
     // actions - the mapping of action names to their implementation
     actions: {
         alertGreen: (context, event) => {
-            alert('green')
+            console.log('green')
+        },
+        logPeople: () => {
+            console.log('people are stopped')
+        },
+        logCars: () => {
+            console.log('cars are stopped')
         }
     },
 
@@ -34,7 +97,14 @@ const lightMachine = createMachine({
     delays: {},
 
     // guards - the mapping of transition guard (cond) names to their implementation
-    guards: {},
+    guards: {
+        isTooManySwitches: (context, event, { cond }) => {
+
+            // @ts-ignore
+            return context.switches > 7 || context.switches >= cond?.minAmount
+        }
+    }
+    ,
 
     // services - the mapping of invoked service (src) names to their implementation
     services: {}
